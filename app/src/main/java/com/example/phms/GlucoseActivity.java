cfaaf8e0cc22492a724888ade3afcf9937d0a200
mariakato <mxk3025@mavs.uta.edu>
2025-04-24 06:13:45 -0500

    Create GlucoseActivity.java

---
+++ app/src/main/java/com/example/phms/GlucoseActivity.java
@@ -0,0 +1,13 @@
+package com.example.phms;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GlucoseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glucose);  // Set the layout for this activity
+    }
+}
